2eb25ced0f392d3e6cf9974888c5656c505ae357
moretti <132162213+smorettii@users.noreply.github.com>
2024-04-07 12:24:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const quantidade = document.querySelector("#quantidade")
-const resultado = document.querySelector("#resultado")
+const resultado = document.querySelectorAll("#resultado")
 function calcular_preco(robux) {
     const taxaDeConversao = 24 / 1000;
     const precoTotal = robux * taxaDeConversao;
     return precoTotal;
 }
+function calcular_com_taxa(valorDesejado) {
+    const valorNecessario = valorDesejado / 0.70;
+
+    return valorNecessario;
+}
 ;(async () => {
     while (true) {
         if (Number(quantidade.value) > 0) {
-            resultado.style.opacity = '1'
-            resultado.textContent = `${Number(quantidade.value)} robux ficam no total de R$ ${String(calcular_preco(Number(quantidade.value)).toFixed(2)).replace(".",",")}`
+            for (v of resultado) {
+                v.style.opacity = '1'
+            }
+            document.querySelector('.comtaxa').textContent = `${Number(quantidade.value)} robux com taxa ficam no total de R$ ${String(calcular_preco(calcular_com_taxa(Number(quantidade.value))).toFixed(2)).replace(".",",")}`
+            document.querySelector('.semtaxa').textContent = `${Number(quantidade.value)} robux sem taxa ficam no total de R$ ${String(calcular_preco(Number(quantidade.value)).toFixed(2)).replace(".",",")}`
+            
         } else {
-            resultado.style.opacity = '0'
+            for (v of resultado) {
+                v.style.opacity = '0'
+            }
         }
         await new Promise(result => setTimeout(result, 10))
     }
